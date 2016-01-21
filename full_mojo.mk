0ec0d65355c6fe73a62c57572f5f0d0c3d759f1b
c457 <android.c357@gmail.com>
2016-01-20 22:35:48 -0600

    mojo: enable aosp builds

--- full_mojo.mk
+++ full_mojo.mk
@@ -22,7 +22,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from mojo device
 $(call inherit-product, device/madcatz/mojo/device.mk)
 
