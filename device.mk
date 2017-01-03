b543ac559b14ac152473e89684a73903987d7921
c457 <android.c357@gmail.com>
2017-01-03 09:52:23 -0600

    mojo: Inherit default hwui config
    * Flounder sets the same values used here and continues to inherit
    the tablet dalvik config. This appears to fix the bootloop issue
    on Tegra4.

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/madcatz/mojo/mojo-vendor.mk)
